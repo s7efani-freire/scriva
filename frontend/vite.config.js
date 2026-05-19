7ff1f6cdbd34af868378c0433b121eef763ba99f
stefani-teiu <desenvolvimento.web@teiu.com.br>
2026-05-19 14:42:10 -0300

    chore: instala e configura Tailwind CSS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    mkcert()
+  ],
   server: {
+    host: '0.0.0.0',
+    https: true,
     port: 5173,
     proxy: {
       '/api': 'http://localhost:3001'
